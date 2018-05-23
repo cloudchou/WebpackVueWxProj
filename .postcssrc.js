165b03eee51aba314a307f91a82165151c41edd1
CloudChou <zhouyaoyun@shuame.com>
2018-05-23 22:45:10 +0800

    添加postcss-px2rem插件 代码中使用px尺寸可自动专为rem

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,10 +1,17 @@
 // https://github.com/michael-ciniawsky/postcss-load-config
 
 module.exports = {
-  "plugins": {
-    "postcss-import": {},
-    "postcss-url": {},
-    // to edit target browsers: use "browserslist" field in package.json
-    "autoprefixer": {}
-  }
+    "plugins": {
+        "postcss-import": {},
+        "postcss-url": {},
+        // to edit target browsers: use "browserslist" field in package.json
+        "autoprefixer": {},
+        "postcss-px2rem": {
+            baseDpr: 2, // 基于设备的Dpr  
+            threeVersion: false, // 是否生成 @1x, @2x and @3x 三个版本(默认: false)，打开后  
+            remVersion: true, // 是否生成rem版本(默认: true)  
+            remUnit: 14, // rem转换比例 (默认: 75)  
+            remPrecision: 5 // rem保留几位数 (默认: 5)  
+        }
+    }
 }
