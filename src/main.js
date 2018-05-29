8ac509a1863794b27ba868d2872cc0624ded16e2
CloudChou <zhouyaoyun@shuame.com>
2018-05-29 15:05:49 +0800

    添加eruda工具

--- src/main.js
+++ src/main.js
@@ -3,8 +3,29 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+const eruda = () =>
+  import ( /* webpackChunkName: "eruda" */ 'eruda')
 
 Vue.config.productionTip = false
+
+
+// import ElementUI from 'element-ui'
+// import 'element-ui/lib/theme-chalk/index.css'
+
+function prepareErudaTool() {
+  if (process.env.EVN_CONFIG !== 'prod') {
+    // const eruda = request('eruda')
+    let el = document.createElement('div')
+    document.body.appendChild(el)
+    // console.log(eruda())
+    eruda().then(function (eruda) {
+      eruda.init({
+        container: el,
+        tool: ['console', 'elements', 'network']
+      })
+    })
+  }
+}
 
 /* eslint-disable no-new */
 new Vue({
@@ -15,3 +36,5 @@ new Vue({
   },
   template: '<App/>'
 })
+
+prepareErudaTool()
